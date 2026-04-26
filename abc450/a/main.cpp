19aec84558d97ed7989fb971a2bf82f8561c9955
hirame6548 <hirame.6548@gmail.com>
2026-04-26 11:47:39 +0900

    first commit

---
+++ abc450/a/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using ll = long long;
+
+template <typename T>
+void print(const T& x) { cout << x << "\n"; }
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    print("こんにちは");
+    print("AtCoder");
+
+
+    return 0;
+}
---
+++ abc450/f/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+using ll = long long;
+
+int main() {
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    
+    
+
+    return 0;
+}
